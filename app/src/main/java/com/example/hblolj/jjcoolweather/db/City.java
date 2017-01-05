0c8c78f3cb5c499f60d556f60aa054af5b81d188
hblolj <hblolj@gmail.com>
2017-01-05 19:16:15 +0800

    Add Create DataBase And Other Setting

---
+++ app/src/main/java/com/example/hblolj/jjcoolweather/db/City.java
@@ -0,0 +1,47 @@
+package com.example.hblolj.jjcoolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by hblolj on 2017/1/5.
+ */
+
+public class City extends DataSupport{
+
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
